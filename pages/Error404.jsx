ba4d82d5ee7c02e698b315d8b1709213ce549dda
Yeison Daza <yeison@platzi.com>
2017-01-04 22:14:52 -0500

    [add] 404 page

--- pages/Error404.jsx
+++ pages/Error404.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
+const style = {
+  display: 'flex',
+  justifyContent: 'center',
+  margin: '30px',
+};
+
 function Error404() {
   return (
-    <section name="Error404">
-      <h1>Error404</h1>
+    <section
+      name="Error404"
+      style={style}
+    >
+      <img
+        src="http://www.404notfound.fr/assets/images/pages/img/androiddev101.jpg"
+        alt="404"
+      />
     </section>
   );
 }
